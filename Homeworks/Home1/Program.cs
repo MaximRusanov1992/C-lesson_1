2bef6c0e6d6d21c7292fac68178737e44979082f
Maxim Rusanov <makrusanov@gmail.com>
2023-09-23 14:12:28 +0700

    add Homeworks and Home1, solution of the task 2, 4

---
+++ Homeworks/Home1/Program.cs
@@ -0,0 +1,70 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+// System.Console.WriteLine("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 > num2)
+// {
+//     System.Console.WriteLine($"max={num1} " );
+// }
+// if (num2 > num1)
+// {
+//     System.Console.WriteLine($"max={num2} " );
+// }
+// if (num2 == num1)
+// {
+//     System.Console.WriteLine("числа равны");
+// }
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+System.Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2 && num1>num3)
+{
+    System.Console.WriteLine($"max={num1} " );
+}
+if (num2 > num1 && num2>num3)
+{
+    System.Console.WriteLine($"max={num2} " );
+}
+if (num3 > num1 && num3>num2)
+{
+    System.Console.WriteLine($"max={num3} " );
+}
+if (num3 == num1 && num3>num2)
+{
+    System.Console.WriteLine($"max={num3} " );
+}
+if (num2 == num1 && num2>num3)
+{
+    System.Console.WriteLine($"max={num2} " );
+}
+if (num2 == num3 && num2>num1)
+{
+    System.Console.WriteLine($"max={num2} " );
+}
+if (num2 == num1 && num1 == num3)
+{
+    System.Console.WriteLine("числа равны");
+}
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
